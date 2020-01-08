38548ed1b51743eba22874adffbccc81b1057313
Ben West <ben.west.500@gmail.com>
2020-01-08 23:13:18 +0000

    revert to python2 and remove unecessary pkgs

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Shell script used in user-data to pull the repo, install & run ansible.
-
 REPO_LOCATION="https://github.com/w3s7y/minecraft-server"
 
 source /etc/os-release
@@ -10,18 +9,16 @@ mkdir /bootstrap
 if [[ ${ID_LIKE} == "debian" ]]
 then
     apt-get update
-    apt-get install -y python3 python3-pip python-virtualenv git
+    apt-get install -y python-virtualenv git
 else
     yum update
-    yum install -y python3 python3-pip python-virtualenv git
+    yum install -y python-virtualenv git
 fi
 
 git clone ${REPO_LOCATION} /bootstrap/minecraft-server
-virtualenv --python /usr/bin/python3 /bootstrap/minecraft-server/venv
+virtualenv --python /usr/bin/python /bootstrap/minecraft-server/venv
 source /bootstrap/minecraft-server/venv/bin/activate
 pip install ansible
-
-mkdir -p /bootstrap/minecraft-server/ansible/roles
 
 ansible-galaxy role install nolte.minecraft
 ansible-playbook -v -c local /bootstrap/minecraft-server/ansible/minecraft-server-playbook.yml
